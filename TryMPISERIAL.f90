496263e5c21e0d3df97d14d7d6ceadf55abbc865
jedwards4b <jedwards@ucar.edu>
2014-03-14 14:08:56 -0600

    separate netcdf libraries into c and fortran

--- TryMPISERIAL.f90
+++ TryMPISERIAL.f90
@@ -1,10 +1,12 @@
 ! Test for the mct mpiserial stubs library
 ! in which mpi_integer == mpi_real4 and the select will
-! fail to compile.
+! fail to compile. (PGI doesnt complain so add something 
+! it will complain about
 !
 program mpiserial_test
+ implicit none
  include 'mpif.h' 
-
+ integer :: i
  select case(i)
  case(mpi_integer)
  case(mpi_real4)
