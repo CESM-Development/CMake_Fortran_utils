1f03f20b09ba61309696f269437aea96d9d84c79
Jim Edwards <jedwards@ucar.edu>
2018-12-17 14:59:29 -0700

    cleanup

--- mpiexec.cmake
+++ mpiexec.cmake
@@ -1,17 +1,10 @@
 function( add_mpi_test _testName _testExe _testArgs _numProc _timeout)
 
-    if ("${PLATFORM}" STREQUAL "yellowstone" )
-        ###
-        ### note: no space between -n and num_proc for mpirun.lsf on
-        ### yellowstone
-        ###
-        set(MPIEXEC_NPF -n${_numProc})
-        set(EXE_CMD ${EXECCA} ${MPIEXEC} ${MPIEXEC_PREFLAGS} ${_testExe} ${_testArgs} ${MPIEXEC_NPF})
-    elseif ("${PLATFORM}" STREQUAL "cetus" )
+    if ("${PLATFORM}" STREQUAL "cetus" )
         ###
         ###
 				#set(PIO_RUNJOB ${CMAKE_BINARY_DIR}/scripts/pio_runjob.sh)
-        set(REQUIRED_OPTION --block \$ENV{COBALT_PARTNAME}) 
+        set(REQUIRED_OPTION --block \$ENV{COBALT_PARTNAME})
         set(RUNJOB_NPF --np ${_numProc})
         if (DEFINED ENV{BGQ_RUNJOB})
           set(RUNJOB $ENV{BGQ_RUNJOB})
@@ -27,7 +20,3 @@ function( add_mpi_test _testName _testExe _testArgs _numProc _timeout)
     set_tests_properties(${_testName} PROPERTIES TIMEOUT ${_timeout})
 
 endfunction(add_mpi_test)
-
-
-
-
